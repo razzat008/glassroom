66e8b789adba34e35418a48813b9cf64541c4eb2
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-05-04 06:42:58 +0545

    added: fmt.Println("Hello, World!")

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/classroom/v1"
+	"google.golang.org/api/option"
+	"net/http"
+)
+
+func main()  {
+	fmt.Println("Hello, World!")
+}
+
