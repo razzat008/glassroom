ea2f8f5c0b234422c027b3725796e8b14d337625
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-05-05 16:02:12 +0545

    updated: implemented token fetching using oauth

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -15,7 +16,7 @@ import (
 
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
-	tokFile := "client_secret.json"
+	tokFile := "token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
@@ -24,29 +25,76 @@ func getClient(config *oauth2.Config) *http.Client {
 	return config.Client(context.Background(), tok)
 }
 
-// Read the token from a local file in root dir
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
 func tokenFromFile(file string) (*oauth2.Token, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-	tok := &oauth2.Token{}               // struct  to store credentials
-	err = json.NewDecoder(f).Decode(tok) // decoding the contents of the json into tok
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
 	return tok, err
 }
 
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-}
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read credentials file: %v", err)
+	}
 
-// Incase token from local file doesn't work
-func getTokenFromWeb(token *oauth2.Config) *oauth2.Token{
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, classroom.ClassroomCourseworkMeReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
 
-}
+	srv, err := classroom.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to create classroom Client %v", err)
+	}
 
-// Save received token from the web into local file
-func saveToken(path string, token *oauth2.Token)  {
-	fmt.Printf("Saving credentials file to:%s\n",path)
-
+	r, err := srv.Courses.List().PageSize(10).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve courses. %v", err)
+	}
+	if len(r.Courses) > 0 {
+		fmt.Print("Courses:\n")
+		for _, c := range r.Courses {
+			fmt.Printf("%s (%s)\n", c.Name, c.Id)
+		}
+	} else {
+		fmt.Print("No courses found.")
+	}
 }
