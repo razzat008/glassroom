7ae590f0a21223cb194cc01a0a52a00151fbee1c
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-05-06 18:04:13 +0545

    created: implemented flags for the program

---
+++ cmd/glassroom/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	classroomauths "glassroom/classroom_auths"
+	"log"
+)
+
+func main() {
+	setup := flag.Bool("setup", false, "Run setup function to get token from classroomApi")
+	bot := flag.Bool("run", true, "Run the telegram bot")
+
+	flag.Parse()
+
+	switch {
+	case *setup:
+		fmt.Println("Running setup for Google Classroom...")
+		client := classroomauths.GetClient()
+	case *bot:
+		fmt.Println("Running Telegram Bot...")
+		fmt.Printf("Connecting to chatID: %v", 6)
+		if err := telegram_bot.RunBot(); err != nil {
+			log.Fatalf("Bot failed to run:")
+		}
+	}
+
+}
